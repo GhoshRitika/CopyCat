d2d076a07e17a20b2451164638441a0290ece91b
Ritika <ritikaghosh2023@u.northwestern.edu>
2023-08-21 10:14:32 -0500

    Update allegro_driver.cpp

--- allegro_driver/src/allegro_driver.cpp
+++ allegro_driver/src/allegro_driver.cpp
@@ -15,8 +15,6 @@
 #include "allegro_lib/rDeviceAllegroHandCANDef.h"
 #include "allegro_lib/RockScissorsPaper.h"
 #include "rclcpp/rclcpp.hpp"
-#include "std_srvs/srv/empty.hpp"
-#include "std_msgs/msg/float64_multi_array.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 
 
